924a19c2ff52731be33632c9b298d6c1fdec7975
Travis Simpson <travis.simpson@fdmgroup.com>
2023-06-19 16:35:52 +0100

    First commit

---
+++ include/Snake.h
@@ -0,0 +1,34 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <windows.h>
+#include <vector>
+#define WIDTH 50
+#define HEIGHT 25
+
+using namespace std;
+
+class Snake
+{
+    private:
+        COORD pos;
+        int len;
+        int vel;
+        char direction;
+        vector<COORD> body;
+
+    public:
+        Snake(COORD pos, int vel); // constructor
+        void change_dir(char dir); // change direction of snake
+        void move_snake(); // move snake in current direction
+        COORD get_pos(); // get current position of snake
+
+        bool eaten(COORD food_pos);
+
+        vector<COORD> get_body();
+
+        void grow();
+        bool collided();
+};
+
+#endif // SNAKE_H
---
+++ src/Snake.cpp
@@ -0,0 +1,54 @@
+#include "Snake.h"
+
+Snake::Snake(COORD pos, int vel)
+{
+    this->pos = pos;
+    this->vel = vel;
+    len = 1;
+    direction = 'n';
+
+    body.push_back(pos);
+}
+
+void Snake::change_dir(char dir) { direction = dir; }
+
+vector<COORD> Snake::get_body() { return body; }
+
+void Snake::move_snake()
+{
+    switch(direction)
+    {
+        case 'u': pos.Y -= vel; break;
+        case 'd': pos.Y += vel; break;
+        case 'l': pos.X -= vel; break;
+        case 'r': pos.X += vel; break;
+    }
+
+    if(pos.X > WIDTH - 2) pos.X = 1;
+    if(pos.X < 1) pos.X = WIDTH - 2;
+    if(pos.Y > HEIGHT - 2) pos.Y = 1;
+    if(pos.Y < 1) pos.Y = HEIGHT - 2;
+
+    body.push_back(pos);
+    if(body.size() > len) body.erase(body.begin());
+}
+
+COORD Snake::get_pos() { return pos; }
+
+bool Snake::eaten(COORD food_pos)
+{
+    if(food_pos.X == pos.X && food_pos.Y == pos.Y) return true;
+    return false;
+}
+
+void Snake::grow() { len++; }
+
+bool Snake::collided()
+{
+    //if(pos.X < 1 || pos.X > WIDTH-2 || pos.Y < 1 || pos.Y > HEIGHT-2) return true;
+    for(int i = 0; i < len-1; i++)
+    {
+        if(pos.X == body[i].X && pos.Y == body[i].Y) return true;
+    }
+    return false;
+}
